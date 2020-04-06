71ba7c13046169525977b27ffe16b815be88d95d
maxchehab <maxchehab@gmail.com>
2020-04-06 05:45:36 +0000

    Update greeter.tsx

--- src/example/greeter.tsx
+++ src/example/greeter.tsx
@@ -5,12 +5,10 @@ import { PheliaMessageProps, Section, Actions, Button, Text } from "../core";
 export default function Greeter({ useState }: PheliaMessageProps) {
   const [name, setName] = useState("name");
 
-  const text = name ? `Hello ${name}` : "Click the button";
-
   return (
     <>
       <Section>
-        <Text>{text}</Text>
+        <Text>{name ? `Hello ${name}` : "Click the button"}</Text>
       </Section>
       <Actions>
         <Button value="greet" onClick={(user) => setName(user.username)}>
